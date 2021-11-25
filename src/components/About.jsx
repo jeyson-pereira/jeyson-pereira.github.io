9d68009dc2bfbca2ea7fe6b8506d053380a75fd0
jeyson-pereira <jpereiraf96@gmail.com>
2021-11-25 01:11:45 -0500

    about section edited

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Typical from 'react-typical'
+import { UserCircleIcon } from '@heroicons/react/outline'
+
 import { phrases } from '../static/data'
 
 import ButtonLink from './ButtonLink'
 
 export default function About() {
   return (
-    <section id="about">
+    <div>
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <div className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Jeyson
             <br className="hidden lg:inline-block" />
             <h1 className="flex-wrap">
               And I love
               <Typical steps={phrases} loop={Infinity} />
             </h1>
-          </h1>
+          </div>
           <p className="mb-8 leading-relaxed">
             Student of Software Development at{' '}
             <a
@@ -44,12 +46,26 @@ export default function About() {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            src="./assets/img/coding.svg"
-            className="object-cover object-center rounded"
+            src="./assets/img/typing.svg"
+            className="object-cover object-center pointer-events-none"
             alt="programmer coding"
           />
         </div>
       </div>
-    </section>
+      <section id="about">
+        <div className="container mx-auto px-5 py-5">
+          <div className="text-center mb-10">
+            <UserCircleIcon className="w-10 inline-block mb-4" />
+            <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+              About Me
+            </h1>
+          </div>
+          <p className="text-center md:px-20">
+            I'm a <b>front-end developer</b> focused on creating{' '}
+            <b>beautiful web and mobile apps</b> writing with <b>clean code</b>.
+          </p>
+        </div>
+      </section>
+    </div>
   )
 }
